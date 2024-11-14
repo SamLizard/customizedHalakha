5fa1b6bb7943a6e9c61a5456c142a2c0669bfcc0
Ssam01 <90714263+SamLizard@users.noreply.github.com>
2024-11-14 07:33:49 +0200

    feat(HalakhaView): trying to add history

--- src/views/HalakhaView.vue
+++ src/views/HalakhaView.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <div>
+      <div>
+        <v-btn icon="mdi-arrow-left" @click="goBackward" style="float: left;"></v-btn>
+        <v-btn icon="mdi-arrow-right" @click="goForward" style="float: right;"></v-btn>
+      </div>
       <!-- <halakha-question :data="trees[id].answers[0]" @select="answerSelection"></halakha-question> -->
       <halakha-question :data="currentData" @select="answerSelection"></halakha-question>
     </div>
@@ -14,11 +18,39 @@ import { defineProps, ref, type Ref } from 'vue';
 
 const props = defineProps<{ id: string }>();
 
-const currentData: Ref<object> = ref(trees[props.id]);
-const answersHistory: Ref<Array<string>> = ref([]);
+var currentIndex: number = 0;
+const answersHistory: Ref<Array<object>> = ref([trees[props.id]]);
+const currentData: Ref<object> = ref(answersHistory.value[0]);
 
 const answerSelection = (index: number) => {
-  currentData.value = currentData.value.answers[index];
-  answersHistory.value.push(index);
-}
+  answersHistory.value.push(currentData.value.answers[index]);
+  currentIndex++;
+  currentData.value = answersHistory.value[currentIndex]; // make it computed???
+};
+
+const goBackward = () => { 
+  if (currentIndex > 0) {
+    currentIndex--;
+    currentData.value = answersHistory.value[currentIndex];
+  }
+};
+
+const goForward = () => { 
+  if (currentIndex < answersHistory.value.length - 1) {
+    currentIndex++;
+    currentData.value = answersHistory.value[currentIndex];
+  }
+};
+
+// const goBackward = () => {
+//   if (answersHistory.value.length > 1) {
+//     currentData.value = answersHistory.value[answersHistory.value.length - 2];
+//   }
+// }
+
+// const goToHistory = (historyPosition) => {
+//   for (let i = 0; i < historyPosition; i++) {
+
+//   }
+// } 
 </script>
